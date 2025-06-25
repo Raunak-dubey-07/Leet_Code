ada2caf5fd65d33cd6463ccb61d2060cfee02483
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2025-06-25 16:19:18 +0530

    Time: 2790 ms (5.02%) | Memory: 240.4 MB (66.81%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -1,41 +1,35 @@
 class Solution {
- int solveMem(int index,int diff,vector<int> &nums, unordered_map<int,int> dp[])
-    {
-        //backward check
-        if(index<0) return 0;
-
-        if(dp[index].count(diff)) 
-           return dp[index][diff];
-
+    public:
+ int solve(vector<int>&nums,int diff,int index,unordered_map<int,int>dp[]){
+        if(index<0){
+            return 0;
+        }
+        if(dp[index].count(diff)){
+            return dp[index][diff];
+        }
+        int j=index-1;
         int ans=0;
-
-        for(int j=index-1;j>=0;j--)
-        {
-            if(nums[index]-nums[j]==diff)
-            {
-                ans=max(ans,1+solveMem(j,diff,nums,dp));
+        for(int j=index-1;j>=0;j--){
+            if(nums[index]-nums[j]==diff){
+                ans=max(ans,1+solve(nums,diff,j,dp));
             }
         }
-
         return dp[index][diff]=ans;
     }
-public:
     int longestArithSeqLength(vector<int>& nums) {
-         int n=nums.size();
-         if(n<=2) return n;
-
-         int ans=0;
-
-         unordered_map<int,int> dp[n+1];
-
-         for(int i=0;i<n;i++)
-         {
-             for(int j=i+1;j<n;j++)
-             {
-                 ans=max(ans,2+solveMem(i,nums[j]-nums[i],nums,dp));
-             }
-         }
-         return ans;
-
+        int n=nums.size();
+        if(n<=2){
+            return n;
+        }
+        unordered_map<int,int>dp[n+1];
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+               // int diff=nums[j]-nums[i];
+                ans=max(ans,2+solve(nums,nums[j]-nums[i],i,dp));
+                //cout<<ans<<endl;
+            }
+        }
+        return ans;
     }
 };
