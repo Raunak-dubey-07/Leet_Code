3642220c61c5c09b89a4105de0f10c7c8906e65d
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2025-06-25 16:32:06 +0530

    Time: 1132 ms (65.21%) | Memory: 245.9 MB (62.22%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -1,21 +1,5 @@
 class Solution {
-    public:
- int solve(vector<int>&nums,int diff,int index,unordered_map<int,int>dp[]){
-        if(index<0){
-            return 0;
-        }
-        if(dp[index].count(diff)){
-            return dp[index][diff];
-        }
-        int j=index-1;
-        int ans=0;
-        for(int j=index-1;j>=0;j--){
-            if(nums[index]-nums[j]==diff){
-                ans=max(ans,1+solve(nums,diff,j,dp));
-            }
-        }
-        return dp[index][diff]=ans;
-    }
+public:
     int longestArithSeqLength(vector<int>& nums) {
         int n=nums.size();
         if(n<=2){
@@ -23,11 +7,15 @@ class Solution {
         }
         unordered_map<int,int>dp[n+1];
         int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-               // int diff=nums[j]-nums[i];
-                ans=max(ans,2+solve(nums,nums[j]-nums[i],i,dp));
-                //cout<<ans<<endl;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+               int diff=nums[i]-nums[j];
+               int cnt=1;
+               if(dp[j].count(diff)){
+                cnt=dp[j][diff];
+               }
+               dp[i][diff]=1+cnt;
+               ans=max(ans,dp[i][diff]);
             }
         }
         return ans;
