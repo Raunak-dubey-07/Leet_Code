66bf0b73010cd6ab2e90d2370f2b19f5381ec70d
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2025-07-07 12:42:15 +0530

    Time: 7 ms (62.67%) | Memory: 18.1 MB (51.21%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,27 +1,48 @@
 class Solution {
 public:
-    int solve(vector<int>& prices,int idx,bool buy,int limit,vector<vector<vector<int>>>&dp){
-        if(idx>=prices.size() || limit==0){
+    int solve(vector<int>& prices, int idx, bool buy, int limit,
+              vector<vector<vector<int>>>& dp) {
+        if (idx >= prices.size() || limit == 0) {
             return 0;
         }
-        if(dp[idx][buy][limit]!=-1){
+        if (dp[idx][buy][limit] != -1) {
             return dp[idx][buy][limit];
         }
-        if(buy){
-            int inc=(-1)*prices[idx]+solve(prices,idx+1,0,limit,dp);
-            int ex=solve(prices,idx+1,1,limit,dp);
-            return dp[idx][buy][limit]=max(inc,ex);
-        }
-        else{
-            int inc=prices[idx]+solve(prices,idx+1,1,limit-1,dp);
-            int ex=solve(prices,idx+1,0,limit,dp);
-            return dp[idx][buy][limit]=max(inc,ex);
+        if (buy) {
+            int inc = (-1) * prices[idx] + solve(prices, idx + 1, 0, limit, dp);
+            int ex = solve(prices, idx + 1, 1, limit, dp);
+            return dp[idx][buy][limit] = max(inc, ex);
+        } else {
+            int inc = prices[idx] + solve(prices, idx + 1, 1, limit - 1, dp);
+            int ex = solve(prices, idx + 1, 0, limit, dp);
+            return dp[idx][buy][limit] = max(inc, ex);
         }
     }
+   int solveTab(vector<int>& prices,int k) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(
+            n+1, vector<vector<int>>(2, vector<int>(k + 1, 0)));
+        for (int idx = n - 1; idx >= 0; idx--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                for (int limit = 1; limit <= k; limit++) {
+                    int profit = 0;
+                    if (buy) {
+                        int inc = (-1) * prices[idx] + dp[idx + 1][0][limit];
+                        int ex = dp[idx + 1][1][limit];
+                        profit = max(inc, ex);
+                    } else {
+                        int inc = prices[idx] + dp[idx + 1][1][limit - 1];
+                        int ex = dp[idx + 1][0][limit];
+                        profit = max(inc, ex);
+                    }
+                    dp[idx][buy][limit]=profit;
+                }
+            }
+        }
+        return dp[0][1][k];
+    }
     int maxProfit(int k, vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(k+1,-1)));
-        return solve(prices,0,1,k,dp);
-        
+
+        return solveTab(prices,k);
     }
 };
