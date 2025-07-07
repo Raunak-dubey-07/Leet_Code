ba21a55d9e494e1d6b407147d3d91f025a4214c7
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2025-07-07 12:24:31 +0530

    Time: 390 ms (53.26%) | Memory: 211.4 MB (52.88%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -19,10 +19,33 @@ public:
         }
         return 0;
     }
-    int maxProfit(vector<int>& prices) {
+    int solveTab(vector<int>& prices){
         int n=prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return solve(prices,0,1,2,dp);
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                for(int k=1;k<=2;k++){
+                     int profit=0;
+                    if(j){
+                       
+                        int inc=(-1)*prices[i]+dp[i+1][0][k];
+                        int ex=dp[i+1][1][k];
+                        profit=max(inc,ex);
+
+                    }
+                    else{
+                        int inc=prices[i]+dp[i+1][1][k-1];
+                        int ex=dp[i+1][0][k];
+                        profit=max(inc,ex);
+                    }
+                    dp[i][j][k]=profit;
+                }
+            }
+        }
+        return dp[0][1][2];
+    }
+    int maxProfit(vector<int>& prices) {
+        return solveTab(prices);
         
     }
 };
