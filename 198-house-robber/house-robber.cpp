88cea25a1fa4814b53d35cad58aeac41308b60a5
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2025-08-13 16:22:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (22.56%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int solve(vector<int>&nums , int idx,vector<int>&dp){
+        if(idx>=nums.size()){
+            return 0;
+        }
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+    int incl=nums[idx]+solve(nums,idx+2,dp);
+    int ex=solve(nums,idx+1,dp);
+    return dp[idx]=max(incl,ex);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        return solve(nums,0,dp);
+        
+    }
+};
