2b8b27afa30109bde945c3052cb01d239fea2c5d
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2025-08-22 21:23:30 +0530

    Time: 2279 ms (5.26%) | Memory: 88.1 MB (53.95%) - LeetSync

---
+++ 2655-rearrange-array-to-maximize-prefix-score/rearrange-array-to-maximize-prefix-score.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxScore(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end(),greater<int>());
+        long long sum=0;
+        int i=0;
+        while(sum>=0 && i<n){
+            sum+=nums[i];
+            cout<<"sum "<<sum<<endl;
+            cout<<nums[i]<<endl;
+            if(sum<=0){
+                break;
+            }
+            i++;
+        }
+        return i;
+        
+    }
+};
