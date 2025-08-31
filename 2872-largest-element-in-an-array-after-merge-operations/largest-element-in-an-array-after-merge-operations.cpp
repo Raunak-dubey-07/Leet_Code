eae7fefe45d4b7d7e572888c0fe94739584b6123
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2025-08-31 16:47:29 +0530

    Time: 0 ms (100.00%) | Memory: 110.5 MB (88.85%) - LeetSync

--- 2872-largest-element-in-an-array-after-merge-operations/largest-element-in-an-array-after-merge-operations.cpp
+++ 2872-largest-element-in-an-array-after-merge-operations/largest-element-in-an-array-after-merge-operations.cpp
@@ -3,21 +3,16 @@ public:
     long long maxArrayValue(vector<int>& nums) {
         int n=nums.size();
         int i=n-1;
-        vector<long long>num(n,0);
-        for(int i=0;i<n;i++){
-            num[i]=nums[i];
-        }
-        while(i>0){
-            if(num[i]>=num[i-1]){
-                num[i-1]=(long long)(num[i]+num[i-1]);
+       long long ans=0;
+        while(i>=0){
+            if(nums[i]<=ans){
+                ans=(long long)(nums[i]+ans);
+            }
+            else{
+                ans=nums[i];
             }
             i--;
         }
-        long long maxi=0;
-        for(int i=0;i<n;i++){
-            maxi=max(maxi,1LL*num[i]);
-        }
-        return maxi;
-        
+        return ans;
     }
 };
