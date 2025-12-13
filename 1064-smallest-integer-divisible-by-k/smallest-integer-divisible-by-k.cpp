4c552896a9a40781d941b42deff804abf4cbd9ef
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2025-12-13 10:59:22 +0530

    Time: 404 ms (5.06%) | Memory: 7.9 MB (23.13%) - LeetSync

---
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) {
+        if(k%2==0 || k%5==0){
+            return -1;
+        }
+        long long rem=0;
+        for(int i=1;i<=k;i++){
+            rem=(rem*10+1)%k;
+            cout<<rem<<endl;
+            if(rem%k==0){
+                return i;
+            }
+        }
+        return -1;
+        
+    }
+};
