a8019ca0ca316c4681cf79ed5bcb581bf54c0458
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2025-12-17 19:39:05 +0530

    Time: 10 ms (44.56%) | Memory: 19.3 MB (71.16%) - LeetSync

--- 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int minSteps(string s, string t) {
         int n=s.size();
-        unordered_map<int,int>mp1;
-        unordered_map<int,int>mp2;
+        vector<int>mp1(26,0);
+        vector<int>mp2(26,0);
         for(int i=0;i<n;i++){
-            mp1[s[i]]++;
-            mp2[t[i]]++;
+            mp1[s[i]-'a']++;
+            mp2[t[i]-'a']++;
         }
         long long ans=0;
         for(int i=0;i<n;i++){
-            if(mp1[s[i]]>0){
-                if(mp1[s[i]]>mp2[s[i]]){
-                    ans+=mp1[s[i]]-mp2[s[i]];
-                    mp1[s[i]]=0;
+            if(mp1[s[i]-'a']>0){
+                if(mp1[s[i]-'a']>mp2[s[i]-'a']){
+                    ans+=mp1[s[i]-'a']-mp2[s[i]-'a'];
+                    mp1[s[i]-'a']=0;
                 }
                
             }
