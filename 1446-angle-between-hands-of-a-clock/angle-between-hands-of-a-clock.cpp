3bab0c5069a82939ae34492bbf48314c4f143cf3
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2025-12-18 21:02:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (10.17%) - LeetSync

--- 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
+++ 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
@@ -10,11 +10,7 @@ public:
          if(min>=360){
             min-=360;
         }
-        double x=abs(hours-min);
-        cout<<x<<endl;
-        double y=abs((360-hours)-(360-min));
-        cout<<y<<endl;
-       double mini = (x < y) ? x : y;
+        double mini=abs(hours-min);
        double mini2=360-mini;
         return (mini < mini2) ? mini : mini2;
     }
