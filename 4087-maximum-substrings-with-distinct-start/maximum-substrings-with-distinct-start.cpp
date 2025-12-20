737ee8411b47b6d1c8aeed342157cf8f928b587d
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2025-12-20 18:34:37 +0530

    Time: 35 ms (36.90%) | Memory: 25.3 MB (31.61%) - LeetSync

---
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxDistinct(string s) {
+        unordered_map<int,int>mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+
+        }
+        return mp.size(); 
+    }
+};
