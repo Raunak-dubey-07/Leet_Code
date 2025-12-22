c8dc140bf789a80b6b15897960e010caca9962e1
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2025-12-22 18:02:12 +0530

    Time: 0 ms (100.00%) | Memory: 77.2 MB (23.94%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -6,7 +6,6 @@ public:
         int n=customers.size();
         for(int i=0;i<n;i++){
             busy=max(busy+customers[i][1],customers[i][1]+customers[i][0]);
-            cout<<busy<<endl;
             waiting+=(busy-customers[i][0]);
         }
         return waiting/n;
