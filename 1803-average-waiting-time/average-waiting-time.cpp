4eb9bb1e9258ffd6e3ae6a41fa1471e59cd1f635
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2025-12-22 18:01:21 +0530

    Time: 446 ms (5.27%) | Memory: 77.3 MB (23.94%) - LeetSync

---
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+        int busy=customers[0][0];
+        double waiting=0;
+        double n=customers.size();
+        for(int i=0;i<n;i++){
+            busy=max(busy+customers[i][1],customers[i][1]+customers[i][0]);
+            cout<<busy<<endl;
+            waiting+=(busy-customers[i][0]);
+        }
+        return waiting/n;
+        
+    }
+};
