5e55efe52ca1f36d0ea580b16b4947eb5466e10f
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2025-12-26 11:18:55 +0530

    Time: 15 ms (1.78%) | Memory: 18.7 MB (99.53%) - LeetSync

--- 983-validate-stack-sequences/validate-stack-sequences.cpp
+++ 983-validate-stack-sequences/validate-stack-sequences.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        int i=0;
-        int j=0;
-        while(i<(int)pushed.size()){
-            // cout<<pushed.size()<<endl;
-            // cout<<i<<endl;
-            if(i<0){
+        int i = 0;
+        int j = 0;
+        while (i < (int)pushed.size()) {
+
+            if (i < 0) {
                 i++;
-            }
-            else if(pushed[i]==popped[j]){
-                cout<<pushed[i]<<endl;
-                pushed.erase(pushed.begin()+i);
+            } else if (pushed[i] == popped[j]) {
+                cout << pushed[i] << endl;
+                pushed.erase(pushed.begin() + i);
                 j++;
                 i--;
-            }
-            else{
+            } else {
                 i++;
             }
-            // cout<<"hii "<<i<<endl;
-            // cout<<pushed.size()<<endl;
-            // cout<<i<<endl;
-            // if(i<pushed.size()){
-            //     cout<<"yes<"<<endl;
-            // }
         }
-        if(pushed.size()==0){
+        if (pushed.size() == 0) {
             return true;
         }
         return false;
-        
     }
 };
