50a1276ea461d51337322b456ce52cff40c0d343
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2026-01-05 20:12:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (67.56%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -6,33 +6,28 @@ public:
         for (int i = 0; i < flights.size(); i++) {
             mp[flights[i][0]].push_back({flights[i][1], flights[i][2]});
         }
-        queue<pair<int,int>> q;
-        q.push({src,0});
+        queue<pair<int, int>> q;
+        q.push({src, 0});
         vector<int> dist(n, INT_MAX);
         dist[src] = 0;
         int step = 0;
-        while (!q.empty() && step<=k) {
+        while (!q.empty() && step <= k) {
             int n = q.size();
-            while (n--){
+            while (n--) {
                 auto front = q.front();
-            q.pop();
-            for (auto& child : mp[front.first]) {
-                cout << "child "<<child.first << endl;
-                if ((front.second + child.second) < dist[child.first]) {
-                    dist[child.first] = front.second + child.second;
-                    q.push({child.first,dist[child.first]});
-                    //cout << dist[child.first] << endl;
+                q.pop();
+                for (auto& child : mp[front.first]) {
+                    if ((front.second + child.second) < dist[child.first]) {
+                        dist[child.first] = front.second + child.second;
+                        q.push({child.first, dist[child.first]});
+                    }
                 }
-    
             }
+            step++;
         }
-        step++;
+        if (dist[dst] != INT_MAX) {
+            return dist[dst];
+        }
+        return -1;
     }
-    if(dist[dst]!=INT_MAX){
-        return dist[dst];
-    }
-    return -1;
-    
-}
-}
-;
+};
