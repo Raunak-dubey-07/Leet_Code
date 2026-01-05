9131ceaefa616e114a29da7d397ef34580400661
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2026-01-05 16:21:59 +0530

    Time: 4 ms (32.55%) | Memory: 39.1 MB (88.21%) - LeetSync

---
+++ 2089-maximum-matrix-sum/maximum-matrix-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        int neg=0;
+        int mini=INT_MAX;
+        long long sum=0;
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]<0){
+                    neg++;
+                }
+                mini=min(mini,abs(matrix[i][j]));
+                sum+=abs(matrix[i][j]);
+            }
+        }
+        if(neg%2!=0){
+            sum-=2*mini;
+        }
+        return sum;
+        
+    }
+};
