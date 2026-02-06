00e5aed8ede431f2d2831db9fbd613c86c2cb9d4
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2026-02-06 11:57:15 +0530

    Time: 7 ms (9.66%) | Memory: 16.4 MB (5.12%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,37 +1,35 @@
-
 class Solution {
 public:
-    int solve(vector<vector<int>>& matrix,int row,int col,vector<vector<int>>&dp){
-        int n=matrix.size();
-        int m=matrix[0].size();
-        if (row < 0 || row >= n || col < 0 || col >= m){
+    int solve(vector<vector<int>>& matrix,int i,int j,vector<vector<int>>&dp,int n,int m){
+        
+        if(i<0 || i>=n || j<0 || j>=m){
             return INT_MAX;
         }
-        if(row==n-1){
-            return matrix[row][col];
+        if(i==n-1){
+           return dp[i][j] = matrix[i][j];
         }
-        if(dp[row][col]!=INT_MAX){
-            return dp[row][col];
+        if(dp[i][j]!=INT_MAX){
+            return dp[i][j];
         }
-        int rightd=solve(matrix,row+1,col+1,dp);
-        int leftd=solve(matrix,row+1,col-1,dp);
-        int bottom=solve(matrix,row+1,col,dp);
-        int next=min(rightd,min(leftd,bottom));
-        if(next==INT_MAX){
-            return dp[row][col]=INT_MAX;
+        int down=solve(matrix,i+1,j,dp,n,m);
+        int left=solve(matrix,i+1,j-1,dp,n,m);
+        int right=solve(matrix,i+1,j+1,dp,n,m);
+        int ans=min({down,left,right});
+        if(ans==INT_MAX){
+            return dp[i][j]=ans;
         }
-        return dp[row][col]= matrix[row][col]+next;
+        return dp[i][j]=ans+matrix[i][j];
+
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
-        int mini=INT_MAX;
         vector<vector<int>>dp(n,vector<int>(m,INT_MAX));
-        for(int i=0;i<m;i++){
-            mini=min(mini,solve(matrix,0,i,dp));
+        int ans=INT_MAX;
+        for(int j=0;j<m;j++){
+            ans=min(ans,solve(matrix,0,j,dp,n,m));
         }
-        return mini;
-
+        return ans;
         
     }
 };
