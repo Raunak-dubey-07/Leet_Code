a1e22857ff245c266f47fdb59e41d8ab3c6c14b2
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2026-02-07 19:11:48 +0530

    Time: 13 ms (18.09%) | Memory: 16.6 MB (57.33%) - LeetSync

---
+++ 1105-uncrossed-lines/uncrossed-lines.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(vector<int>& nums1, vector<int>& nums2,int i,int j,int n,int m,vector<vector<int>>&dp){
+    if(i>=n || j>=m){
+        return 0;
+    }
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    if(nums1[i]==nums2[j]){
+        return dp[i][j]=1+solve(nums1,nums2,i+1,j+1,n,m,dp);
+    }
+    int ans1=solve(nums1,nums2,i+1,j,n,m,dp);
+    int ans2=solve(nums1,nums2,i,j+1,n,m,dp);
+    return dp[i][j]=max(ans1,ans2);
+    }
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
+        int m=nums2.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return solve(nums1,nums2,0,0,n,m,dp);
+    }
+};
