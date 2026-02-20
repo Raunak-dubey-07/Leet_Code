8bcd7f67be8073ab62063179aed6d4adf49dbedc
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2026-02-20 17:59:25 +0530

    Time: 239 ms (7.46%) | Memory: 415.2 MB (5.06%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public: 
-    bool check(unordered_map<char,int>mp1,unordered_map<char,int>mp2,string p){
-        for(int i=0;i<p.size();i++){
-            if(mp1[p[i]]!=mp2[p[i]]){
+    bool check(vector<int>mp1,vector<int>mp2,string p){
+        for(int i=0;i<26;i++){
+            if(mp1[i]!=mp2[i]){
                 return false;
             }
         }
         return true;
     }
     vector<int> findAnagrams(string s, string p) {
-        unordered_map<char,int>mp1;
+        vector<int>mp1(26,0);
         int n=p.size();
         for(int i=0;i<n;i++){
-            mp1[p[i]]++;
+            mp1[p[i]-'a']++;
         }
-        unordered_map<char,int>mp2;
+        vector<int>mp2(26,0);
         int j=0;
         int m=s.size();
         int i=0;
         vector<int>res;
         while(j<m){
-            mp2[s[j]]++;
-            while(mp2[s[j]]>mp1[s[j]]){
-                mp2[s[i]]--;
-                if(mp2[s[i]]==0){
-                    mp2.erase(s[i]);
-                }
+            mp2[s[j]-'a']++;
+            while(mp2[s[j]-'a']>mp1[s[j]-'a']){
+                mp2[s[i]-'a']--;
                 i++;
             }
-            if(mp1.size()==mp2.size()&&check(mp1,mp2,p)){
+            if(check(mp1,mp2,p)){
                 res.push_back(i);
             }
             j++;
