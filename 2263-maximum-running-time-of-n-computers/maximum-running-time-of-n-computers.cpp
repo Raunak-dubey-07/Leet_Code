8935396f851b745dccbd5d2352244ef603a1125d
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2026-03-27 22:03:54 +0530

    Time: 21 ms (61.10%) | Memory: 59.8 MB (55.58%) - LeetSync

---
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool check(long long n,vector<int>& batteries,long long mid){
+        long long total=0;
+        int m=batteries.size();
+        
+        for(int i=0;i<m;i++){
+            total+=min(mid,1LL*batteries[i]);
+           
+        }
+        return total>=n*mid;
+    }
+    long long maxRunTime(int n, vector<int>& batteries) {
+        long long l=0;
+        long long r=accumulate(batteries.begin(),batteries.end(),0LL);
+        long long ans=0;
+
+        while(l<=r){
+            long long mid=l+(r-l)/2;
+            if(check(n,batteries,mid)){
+                ans=mid;
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+
+        }
+        return ans; 
+    }
+};
