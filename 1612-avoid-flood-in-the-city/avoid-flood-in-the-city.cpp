7b23bf4993f04348d55f7a0c27396716e6e8e864
Raunak-dubey-07 <rounakdubey2009@gmail.com>
2026-04-12 11:22:13 +0530

    Time: 157 ms (62.59%) | Memory: 155.1 MB (8.34%) - LeetSync

--- 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
+++ 1612-avoid-flood-in-the-city/avoid-flood-in-the-city.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     vector<int> avoidFlood(vector<int>& rains) {
-        vector<long long>dry;
+        set<long long>dry;
         unordered_map<long long,long long>mp;
         int n=rains.size();
         vector<int>ans(n,-1);
         for(int i=0;i<n;i++){
             if(rains[i]==0){
-                dry.push_back(i);
+                dry.insert(i);
             }
             else if(mp.find(rains[i])==mp.end()){
                 mp[rains[i]]=i;
             }
             else{
                 long long x=mp[rains[i]];
-                long long idx=lower_bound(dry.begin(),dry.end(),x)-dry.begin(); 
-                if(idx>=dry.size()){
-                    return { };
-                }
-                ans[dry[idx]]=rains[i];
-                dry.erase(dry.begin()+idx);
+                auto it = dry.lower_bound(x);
+                if(it == dry.end())
+                    return {};
+
+                ans[*it] = rains[i];
+                dry.erase(it);
                 mp[rains[i]]=i;
             }
           
